53921bfd036aa41eb0e75de63e460d9a235cc826
Mary Le <maaaryle@gmail.com>
2014-10-30 18:05:56 -0700

    made easy folder; add firstFactorial

---
+++ Easy/02-firstFactorial.js
@@ -0,0 +1,10 @@
+var fact = function(num){
+var result = num;
+for (var i = 1; i < num; i++){
+     result = result * [i];
+    } return result;
+};
+
+fact(4); // 24
+fact(5); // 120
+fact(8); // 40320
